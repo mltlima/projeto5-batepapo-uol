36ddca9f51e1ca7bb0bd796358190c4be7b167e7
mltlima <miguellt@dcc.ufrj.br>
2022-02-11 00:13:57 -0300

    rolagem automatica

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let lastMessage = null;
+let isMessagePrintable = true;
+let mainUser = "";
+
 function getDataAPI() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
     
@@ -23,15 +27,15 @@ function enterRoom() {
 
     let correctUserInput = false;
 
-    let input = prompt("Qual o seu lindo nome?");
-
+    mainUser = prompt("Qual o seu lindo nome?");
+    
     while (!correctUserInput) {
         
-        let user = {name: input}
+        let user = {name: mainUser}
         const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants ", user);    
         correctUserInput = promise.then(userSucess);
         if (!correctUserInput) {
-            input = prompt("Nome já esta em uso, digite um novo nome");
+            mainUser = prompt("Nome já esta em uso, digite um novo nome");
         } else {
             //setTimeout(keepUserActive(user),5000);
             setInterval(() => {
@@ -40,7 +44,7 @@ function enterRoom() {
         }
         promise.catch(errorHandle);
     }
-} //enterRoom();
+} enterRoom();
 
 function keepUserActive(user) {
     const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status ", user);    
@@ -56,16 +60,25 @@ function updateMessages() {
         promise.catch(errorHandle);
 
 } 
-//setInterval(() => {
+setInterval(() => {
     updateMessages();
-//},3000)
+},3000)
 
 function messageSucess(data) {
-    console.log(data.data);
+    //console.log(data.data);
     let messages = data.data
     const mainHTML = document.querySelector("main");
 
     for (let i = 0; i < messages.length; i++) {
+
+        if (lastMessage != null) {
+            if (lastMessage.time == messages[i].time && lastMessage.from == messages[i].from && lastMessage.to == messages[i].to && lastMessage.text == messages[i].text && messages.length-1 != i) {
+                lastMessage = null;
+                continue;
+            }
+            continue;
+        }    
+
         let type = messages[i].type;  
         if (type  === "status") {
             mainHTML.innerHTML += `<div class="container-box">
@@ -73,10 +86,21 @@ function messageSucess(data) {
             `
         }
         if (type  === "message") {
-            
+            mainHTML.innerHTML += `<div class="container-box normal-messages">
+            <p><span class="time"> (${messages[i].time})</span> <span class="strong">${messages[i].from}</span> para <span class="strong">Todos:</span> ${messages[i].text}</p>
+            `
         }
-        if (type  === "private_message") {
-            
+        if (type  === "private_message" && messages[i].to === mainUser) {
+            mainHTML.innerHTML += `<div class="container-box private-messages">
+            <p><span class="time"> (${messages[i].time})</span> <span class="strong">${messages[i].from}</span> reservadamente para <span class="strong">${messages[i].to}</span> ${messages[i].text}</p>
+            `
+        }
+
+        document.querySelector(".container-box:last-child").scrollIntoView();
+
+        if (i === messages.length - 1) {
+            lastMessage = messages[i];
+            console.log(lastMessage);
         }
     }
     
