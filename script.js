3cb96f96446b5b5ced1ace0a9a58741948ae65c6
mltlima <miguellt@dcc.ufrj.br>
2022-02-10 22:40:33 -0300

    display mensagens

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function getDataAPI() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
-
+    
     promise.then(sucessHandle);
     promise.catch(errorHandle);
 } 
 
 function sucessHandle(data) {
     console.log(data.data);
+}
+
+function userSucess() {
+    getDataAPI();
+    return true;
 }
 
 function errorHandle(error) {
@@ -29,23 +34,50 @@ function enterRoom() {
             input = prompt("Nome já esta em uso, digite um novo nome");
         } else {
             //setTimeout(keepUserActive(user),5000);
-            setTimeout(keepUserActive(promise),5000);
+            setInterval(() => {
+                keepUserActive(user);
+            }, 5000);
         }
         promise.catch(errorHandle);
     }
-} enterRoom();
+} //enterRoom();
 
-function userSucess(data) {
-    //console.log(data);
-    getDataAPI();
-    return true;
-}
-
-function keepUserActive(promise) {
-    
-    setInterval(() => {
+function keepUserActive(user) {
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status ", user);    
         promise.then(userSucess);
         promise.catch(errorHandle);
-    },5000)   
+}
+
+
+function updateMessages() {
+
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+        promise.then(messageSucess);
+        promise.catch(errorHandle);
+
+} 
+//setInterval(() => {
+    updateMessages();
+//},3000)
+
+function messageSucess(data) {
+    console.log(data.data);
+    let messages = data.data
+    const mainHTML = document.querySelector("main");
+
+    for (let i = 0; i < messages.length; i++) {
+        let type = messages[i].type;  
+        if (type  === "status") {
+            mainHTML.innerHTML += `<div class="container-box">
+            <p><span class="time"> (${messages[i].time})</span> <span class="user">${messages[i].from}</span> ${messages[i].text}</p>
+            `
+        }
+        if (type  === "message") {
+            
+        }
+        if (type  === "private_message") {
+            
+        }
+    }
     
 }
